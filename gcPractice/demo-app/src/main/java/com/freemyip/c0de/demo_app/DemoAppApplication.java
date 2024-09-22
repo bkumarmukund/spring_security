c2a064fbf8fabe0ba039021ff6e925f62bf3e109
Balmukund Kumar <bkumarmukund@gmail.com>
2024-09-23 02:29:39 +0530

    demo project
    no security simple endpoints
    /home
    /home/user
    /home/admin

---
+++ gcPractice/demo-app/src/main/java/com/freemyip/c0de/demo_app/DemoAppApplication.java
@@ -0,0 +1,30 @@
+package com.freemyip.c0de.demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoAppApplication {
+
+
+	@GetMapping("/home")
+	public String home() {
+		return "welcome anynymous user";
+	}
+	@GetMapping("/home/user")
+	public String homeOfUser() {
+		return "welcome logged in user";
+	}
+	@GetMapping("/home/admin")
+	public String homeOfAdmin() {
+		return "welcome admin";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+
+}
